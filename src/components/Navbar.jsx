e8a725a151615dbdc09b942481eacd8d1f788476
temuriee <critstoggle@gmail.com>
2025-12-06 02:25:18 -0800

    .

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,9 @@
+import { Menu, X } from "lucide-react";
+import { useState } from "react";
+
 const Navbar = () => {
+  const [mobileMenuIsOpen, setMobileMenuIsOpen] = useState(false);
+
   return (
     <nav className="fixed top-0 w-full z-50 transition-all duration-300 bg-slate-950/20 backdrop-blur-sm">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8  ">
@@ -18,7 +23,7 @@ const Navbar = () => {
           </div>
 
           {/* Nav Links */}
-          <div className="flex items-center space-x-6 lg:space-x-8 ">
+          <div className="hidden md:flex items-center space-x-6 lg:space-x-8 ">
             <a
               href="#features"
               className="text-gray-300 hover:text-white text-sm lg:text-base"
@@ -38,10 +43,55 @@ const Navbar = () => {
               Testimonials
             </a>
           </div>
+
+          <button
+            className="md:hidden p-2 text-gray-300 hover:text-white "
+            onClick={() => setMobileMenuIsOpen((prev) => !prev)}
+          >
+            {mobileMenuIsOpen ? (
+              <X className="w-5 h-5 sm:w-6 sm:h-6" />
+            ) : (
+              <Menu className="w-5 h-5 sm:w-6 sm:h-6" />
+            )}
+          </button>
         </div>
       </div>
+
+      {mobileMenuIsOpen && (
+        <div
+          className={`md:hidden bg-slate-900/95 backdrop-blur-lg border-t border-slate-800 animate-in  duration-300 slide-in-from-top `}
+        >
+          <div className="  px-4 py-4 sm:py-6 space-y-3 sm:space-y-4 ">
+            <a
+              href="#features"
+              className=" block  text-gray-300 hover:text-white text-sm lg:text-base"
+              onClick={() => setMobileMenuIsOpen(false)}
+            >
+              Features
+            </a>
+            <a
+              href="#pricing"
+              className=" block  text-gray-300 hover:text-white text-sm lg:text-base"
+              onClick={() => setMobileMenuIsOpen(false)}
+            >
+              Pricing
+            </a>
+            <a
+              href="#testimonials"
+              className="block  text-gray-300 hover:text-white text-sm lg:text-base"
+              onClick={() => setMobileMenuIsOpen(false)}
+            >
+              Testimonials
+            </a>
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
 
 export default Navbar;
+
+// ${
+//             mobileMenuIsOpen ? "slide-in-from-top" : "slide-out-top"
+//           }
